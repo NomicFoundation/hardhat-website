3a5c0cb0461096cc1ba654f876b20c52b16f481a
Patricio Palladino <email@patriciopalladino.com>
2025-10-30 17:47:09 +0000

    Add redirects for the hardhat-chai-matchers sections

--- src/redirects/hh2.ts
+++ src/redirects/hh2.ts
@@ -1,3 +1,26 @@
 import type { AstroConfig } from "astro";
 
-export default {} satisfies AstroConfig["redirects"];
+export default {
+  "/hardhat-chai-matchers": {
+    status: 302,
+    destination: "/docs/plugins/nomicfoundationhardhat-ethers-chai-matchers",
+  },
+  "/hardhat-chai-matchers/docs": {
+    status: 302,
+    destination: "/docs/plugins/nomicfoundationhardhat-ethers-chai-matchers",
+  },
+  "/hardhat-chai-matchers/docs/overview": {
+    status: 302,
+    destination: "/docs/plugins/nomicfoundationhardhat-ethers-chai-matchers",
+  },
+  "/hardhat-chai-matchers/docs/reference": {
+    status: 302,
+    destination:
+      "/docs/plugins/nomicfoundationhardhat-ethers-chai-matchers#reference",
+  },
+  "/hardhat-chai-matchers/docs/migrate-from-waffle": {
+    status: 302,
+    destination:
+      "https://v2.hardhat.org/hardhat-chai-matchers/docs/migrate-from-waffle",
+  },
+} satisfies AstroConfig["redirects"];
