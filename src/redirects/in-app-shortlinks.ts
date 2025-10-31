bc099d5c171aef639b1e8654f9acdc4f6f7e6e1b
Patricio Palladino <email@patriciopalladino.com>
2025-10-31 17:45:24 +0000

    Fix in-app-shortlinks

--- src/redirects/in-app-shortlinks.ts
+++ src/redirects/in-app-shortlinks.ts
@@ -1,21 +1,22 @@
 import type { Redirects } from "./types";
 
 export default [
-  [
-    "/hardhat-runner/docs/config#hd-wallet-config",
-    "/docs/reference/configuration#hd-wallet-config",
-  ],
-  [
-    "/hardhat-runner/plugins/nomicfoundation-hardhat-ethers#library-linking",
-    "/docs/plugins/hardhat-ethers#library-linking",
-  ],
-  // TODO
+  // TODO: These should be replaced by shortlinks in the app
+  // [
+  //   "/hardhat-runner/docs/config#hd-wallet-config",
+  //   "/docs/reference/configuration#hd-wallet-config",
+  // ],
+  // [
+  //   "/hardhat-runner/plugins/nomicfoundation-hardhat-ethers#library-linking",
+  //   "/docs/plugins/hardhat-ethers#library-linking",
+  // ],
+  // TODO: They don't have a matching page yet
   // ["/hardhat-network-helpers/docs/reference#fixtures", ""],
   // ["/chaining-async-matchers", ""],
   // ["/console-log", ""],
   [
     "/verify-custom-networks",
-    "/plugin-development/guides/integration-tests#testing-with-fixture-projects",
+    "/docs/guides/smart-contract-verification#verifying-on-a-block-explorer-of-a-different-network",
   ],
   ["/ignition-errors", "/ignition/docs/explanations/error-handling"],
   [
